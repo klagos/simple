812d21d0a82d4a78f4b7d7512324b5e8663de56e
“IST <istdevcl@gmail.com>
2018-02-06 19:51:00 +0100

    costCenter y service no son obligatorios

--- application/models/campo_chosen_usuario.php
+++ application/models/campo_chosen_usuario.php
@@ -14,25 +14,7 @@ class CampoChosenUsuario extends Campo {
 	$display = '<label class="control-label" for="'.$this->id.'">' . $this->etiqueta . (in_array('required', $this->validacion) ? '' : ' (Opcional)') . '</label>';
   
 	$display.= '<div class="controls">';
-        
-	$display.='<script>function setMaximumSelected(amount,element) {
-			var itemsSelected = [];
-			for (var i=0;i<element.options.length;i++) {
-				if (element.options[i].selected) itemsSelected[itemsSelected.length]=i;
-			}
-			if (itemsSelected.length>3) {
-				itemsSelected = element.itemsSelected.split(",");
-				for (i=0;i<element.options.length;i++) {
-					element.options[i].selected = false;
-				}
-				for (i=0;i<itemsSelected.length;i++) {
-					element.options[itemsSelected[i]].selected = true;
-				}			
-			} else {
-				element.itemsSelected=itemsSelected.toString();
-			}
-		}</script>';
-	
+       	
 	if ($this->extra->ws and !$this->datos){
 		$url = $this->extra->ws;
         	$ch = curl_init($url);
@@ -44,33 +26,17 @@ class CampoChosenUsuario extends Campo {
         	    ));
         	$result=curl_exec($ch);
        		curl_close($ch);
-                
-		$placeholder = "";
-                $json_ws = "";
-                $flag_json = false;
-                $flag_placeholder = false;
-
-                for ($i = 0; $i < strlen($result); $i++){
-                        if ($result[$i] and $flag_placeholder){
-                                if ($result[$i] == ":") $flag_placeholder = false;
-                                if ($flag_placeholder) $placeholder .= $result[$i];
-                        }
-                        if ($result[$i] == "[") $flag_json = true; //empieza json
-                        if ($flag_json) $json_ws .= $result[$i];
-                        if ($result[$i] == "{" and !$placeholder) $flag_placeholder = true;
-                        if ($result[$i] == "]") $flag_json = false; //termina json
-                }
-                $placeholder = preg_replace('/\n+/', '',$placeholder); //eliminar espacios de placeholder
-		$json_ws = json_decode($json_ws);
 		
-		$display.='<select size="35" style="width:270px" data-placeholder="Seleccione por rut o nombre"  class="chosen" id= "'.$this->id.'"  name="'.$this->nombre.'" ' . ($modo == 'visualizacion' ? 'readonly' : '') . ' data-modo="'.$modo.'" >';
+		$json_ws = json_decode($result);
+		
+		$display.='<select size="35" style="width:380px" data-placeholder="Seleccione por rut o nombre"  class="chosen" id= "'.$this->id.'"  name="'.$this->nombre.'" ' . ($modo == 'visualizacion' ? 'readonly' : '') . ' data-modo="'.$modo.'" >';
                 $display.='<option value="null"> </option>';
 
                 foreach ($json_ws as $json){
 			if($dato){
-        	               	$display.='<option value="' .$json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location .'-'.$json->costCenter.'-'.$json->service.'"'.($json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location.'-'.$json->costCenter.'-'.$json->service  == $dato->valor ? 'selected' : ' ') .'>'.explode(" ",$json->name)[0].' '.$json->lastName.' - '.$json->rut.'</option>';
+        	               	$display.='<option value="' .$json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location . (isset($json->costCenter)?'-'. $json->costCenter :'').'-'. (isset($json->service)? '-'.$json->service : '')  .'"'.($json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location.'-'.$json->costCenter.  (isset($json->service)?'-'.$json->service : '')  == $dato->valor ? 'selected' : ' ') .'>'.explode(" ",$json->name)[0].' '.$json->lastName.' - '.$json->rut.'</option>';
                        	}else{
-                        $display.='<option value="' .$json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location .'-'.$json->costCenter.'-'.$json->service. '"'.($json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location.'-'.$json->costCenter.'-'.$json->service == $valor_default ? 'selected' : ' ') .'>'.explode(" ",$json->name)[0].' '.$json->lastName.' - '.$json->rut.'</option>';
+                        $display.='<option value="' .$json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location . (isset($json->costCenter)?'-'. $json->costCenter :'') . (isset($json->service)? '-'.$json->service : ''). '"'.($json->lastName."/".$json->name.'-'.$json->rut.'-'.$json->location. (isset($json->costCenter)?'-'. $json->costCenter :'')  . (isset($json->service)?'-'.$json->service : '') == $valor_default ? 'selected' : ' ') .'>'.explode(" ",$json->name)[0].' '.$json->lastName.' - '.$json->rut.'</option>';
                        	}                	
 		}
         }else{
@@ -86,41 +52,6 @@ class CampoChosenUsuario extends Campo {
 			}
 	}
         $display.='</select> <input name"'. $this->nombre.'"id="'.$this->nombre.'" type=hidden value""/>';
-	
-?>
-
-<?php	
-
-	$display.='<script>
-			 $("#'.$this->id.'").chosen({ search_contains: true});
-                        $(document).ready(function(){
-                               
-				$(".'.$this->id.'").chosen({ search_contains: true});	
-				$(".chosen").chosen({
-                                        allow_single_deselect: true,
-					max_selected_options: 1
-                                });
-        
-				function setMaximumSelected(amount,element) {
-			var itemsSelected = [];
-			for (var i=0;i<element.options.length;i++) {
-				if (element.options[i].selected) itemsSelected[itemsSelected.length]=i;
-			}
-			if (itemsSelected.length>3) {
-				itemsSelected = element.itemsSelected.split(",");
-				for (i=0;i<element.options.length;i++) {
-					element.options[i].selected = false;
-				}
-				for (i=0;i<itemsSelected.length;i++) {
-					element.options[itemsSelected[i]].selected = true;
-				}			
-			} else {
-				element.itemsSelected=itemsSelected.toString();
-			}
-		}
-	});
-        </script>';
-
 	
 
 	if($this->ayuda)
