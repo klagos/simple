e586d1a9be8143f3184e5241a16e792be45210b0
“IST <istdevcl@gmail.com>
2018-02-09 16:37:15 +0100

    accion enviar admin days

---
+++ application/models/accion_enviar_admindays.php
@@ -0,0 +1,50 @@
+<?php
+require_once('accion.php');
+require_once('ChromePhp.php');
+
+class AccionEnviarAdminDays extends Accion {
+
+    public function displayForm() {
+
+
+        $display = '<label>Fecha Solicitud</label>';
+        $display.='<input type="text" name="extra[date_request]" value="' . (isset($this->extra->date_request) ? $this->extra->date_request : '') . '" />';
+        $display.= '<label>Tipo Solicitud</label>';
+        $display.='<input type="text" name="extra[type]" value="' . (isset($this->extra->type) ? $this->extra->type : '') . '" />';
+	$display.= '<label>Rut Trabajador</label>';
+        $display.='<input type="text" name="extra[rut]" value="' . (isset($this->extra->rut) ? $this->extra->rut : '') . '" />';	
+	
+        return $display;
+    }
+
+    public function validateForm() {
+        $CI = & get_instance();
+        $CI->form_validation->set_rules('extra[date_request]', 'Fecha Solicitud', 'required');
+        $CI->form_validation->set_rules('extra[type]', 'Tipo Solicitud', 'required');
+	$CI->form_validation->set_rules('extra[rut]', 'Rut', 'required');
+    }
+
+    public function ejecutar(Etapa $etapa) {
+        $regla=new Regla($this->extra->date_request);
+        $date_request=$regla->getExpresionParaOutput($etapa->id);
+        $regla=new Regla($this->extra->type);
+        $type=$regla->getExpresionParaOutput($etapa->id);
+	$regla=new Regla($this->extra->rut);
+        $rut=$regla->getExpresionParaOutput($etapa->id);
+        
+	$json= '{"date_request": '.(isset($date_request) ? '"'.$date_request.'"' : '""').', ';
+        $json.= '"type": '.(isset($type) ? '"'.$type.'"' : '""').'}';
+	
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://private-120a8-apisimpleist.apiary-mock.com/user/".$rut."/admindays");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                "Content-Type: application/json"
+                    ));
+        curl_exec($ch);
+        curl_close($ch);
+    }
+
+}
