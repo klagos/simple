e10f6ddba7557789b517912248f28fa56e417cc2
“IST <istdevcl@gmail.com>
2018-02-12 21:53:45 +0100

    se agrego boton para consultar dias admin

--- application/views/backend/formularios/editar.php
+++ application/views/backend/formularios/editar.php
@@ -30,7 +30,7 @@
         <div class="btn-group">
             <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'text')">Textbox</button>
             <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'textrut')">TextboxRut</button>
-            <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'chosenUsuario')">ChosenUsuario</button>
+            <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'chosenUsuario')">ChosenUsuarioIST</button>
 	    <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'chosenUnitario')">ChosenUnitario</button>
 	    <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'chosenAdd')">ChosenAdd</button>
 	    <button class="btn btn-inverse" onclick="return agregarCampo(<?= $formulario->id ?>,'textarea')">Textarea</button>
