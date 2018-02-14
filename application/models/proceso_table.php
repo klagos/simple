d0a4b03d3972348078bd0ffec44833ae8fe27298
“IST <istdevcl@gmail.com>
2018-02-14 21:47:21 +0100

    se agrego metodo para revisar dias admin

--- application/models/proceso_table.php
+++ application/models/proceso_table.php
@@ -61,6 +61,23 @@ class ProcesoTable extends Doctrine_Table {
                                 unset($procesos[$key]);
                 return (count($procesos)!=0)?true:false;
         }
+	
+	public function canRevisarDiasAdmin($usuario_id){
+                $proceso_dias_admin = proceso_dias_admin_id;
+                $usuario=Doctrine::getTable('Usuario')->find($usuario_id);
+                $query=Doctrine_Query::create()
+                ->from('Proceso p, p.Cuenta c, p.Tareas t')
+                ->where('p.activo=1 AND t.inicial = 1')
+                ->andWhere('p.id=?',$proceso_dias_admin)
+                ->andWhere('1!=(t.activacion="no" OR ( t.activacion="entre_fechas" AND ((t.activacion_inicio IS NOT NULL AND t.activacion_inicio>NOW()) OR (t.activacion_fin IS NOT NULL AND NOW()>t.activacion_fin) )))');
+                $procesos=$query->execute();
+
+                //Chequeamos los permisos de acceso
+                foreach($procesos as $key=>$p)
+                        if(!$p->canUsuarioListarlo($usuario_id))
+                                unset($procesos[$key]);
+                return (count($procesos)!=0)?true:false;
+        }
 
 
 }
