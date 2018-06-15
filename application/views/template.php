b4b3985d0786c1c1ebb9a307d2023fb482e9f88f
“IST <istdevcl@gmail.com>
2018-06-16 00:09:38 +0200

    merge

--- application/views/template.php
+++ application/views/template.php
@@ -116,6 +116,9 @@
 			//Modulo FAS
 			$fas= Doctrine::getTable('GrupoUsuarios')->cantGruposUsuaros(UsuarioSesion::usuario()->id,"MODULO_FAS");
 			
+			//Modulo Vacation
+			$vacation = true;
+			
 			?>
 			<ul id="sideMenu" class="nav nav-list">
 				<li class="iniciar"><a  href="#" onclick="Slide(['Inicio1','Inicio2','Inicio3'])">&nbsp;&nbsp;&nbsp;Inicio<span style="font-size:20px;top:3px" class="pull-left hidden-xs showopacity glyphicon glyphicon-home"></a></li>
@@ -187,7 +190,12 @@
 			<?php if($fas): ?><li class="<?= isset($sidebar) && $sidebar == 'fas_consolidado' ? 'active' : '' ?>"><a id="fas_consolidado" href="<?= site_url('fas/consolidado') ?>">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Consolidado&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <span style="font-size:13px;top:3px;left:28px" class="pull-ccenter hidden-xs showopacity glyphicon glyphicon-briefcase"></a></li><?php endif ?>
 
                         </ul>
-
+			<!-- MODULO VACATION -->
+                        <ul id="sideMenu" class="nav nav-list">
+                           <?php if($vacation): $cont_menu++; ?><li class="iniciar"><a  href="#" onclick="Slide(['vacation_add'])">&nbsp;&nbsp;&nbsp;Feriados<span style="font-size:20px;top:8px" class="pull-left hidden-xs showopacity glyphicon glyphicon-th-large"></span></a></li><?php endif ?>
+                           <?php if($vacation): ?><li class="<?= isset($sidebar) && $sidebar == 'vacation_add' ? 'active' : '' ?>"><a id="vacation_add" href="<?= site_url('tramites/iniciar/'. proceso_vacation) ?>">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Agregar&nbsp;&nbsp;&nbsp;&nbsp;  <span style="font-size:13px;top:3px;left:28px" class="pull-ccenter hidden-xs showopacity glyphicon glyphicon-plus"></a></li><?php endif ?>
+                        <?php if(false): ?><li class="<?= isset($sidebar) && $sidebar == 'fas_add_medico' ? 'active' : '' ?>"><a id="fas_add_medico" href="<?= site_url('tramites/iniciar/'. proceso_fas_social) ?>">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Beneficio Social&nbsp;&nbsp;&nbsp;&nbsp;  <span style="font-size:13px;top:3px;left:28px" class="pull-ccenter hidden-xs showopacity glyphicon glyphicon-plus"></a></li><?php endif ?>
+			</ul>
 
 	
 			<!-- MODULO ESTUDIO PSICOSOCIAL -->
